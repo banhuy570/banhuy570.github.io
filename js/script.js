3eed36e664932bb6850142c0bfedfa5348a12b54
banhuy570 <71781815+banhuy570@users.noreply.github.com>
2021-11-22 09:07:14 +0700

    1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// const menuBar = document.querySelector(".nav_mobile_input")
+// const closeBtn = document.querySelector(".nav_mobile_close")
+// menuBar.addEventListener("click", function() {
+//     document.querySelector(".nav_mobile").style.transform = "translateX(0%)"
+//     document.querySelector(".nav_mobile").style.opacity = "1"
+// })
+// closeBtn.addEventListener("click", function() {
+//     document.querySelector(".nav_mobile").style.transform = "translateX(-100%)"
+//     document.querySelector(".nav_mobile").style.opacity = "0"
+// })
